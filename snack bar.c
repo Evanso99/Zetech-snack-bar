3d1e2b3990ef30556846f8bfe3410baafdae0611
Evanso99 <106368871+Evanso99@users.noreply.github.com>
2022-07-15 23:58:42 +0300

    Add files via upload

---
+++ snack bar.c
@@ -0,0 +1,17 @@
+// File Name: snack bar
+// Created by: Evans Ochieng
+// Reg No: DIT-02-0394/2022
+
+#include <iosteam>
+using namespace std;
+
+int main()
+{   printf("ZETECH SNACK BAR");
+    printf(""smokie = 40
+             Milkshake = 150
+             Harmburger = 300
+             Samosa = 50
+             African = 60
+           "");
+           return 0;
+}
